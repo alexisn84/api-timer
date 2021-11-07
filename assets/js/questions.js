f2899407087a3cc16da0181cbd38deb43dda51bc
alexisn84 <91172382+alexisn84@users.noreply.github.com>
2021-11-07 10:05:57 -0500

    HTML, CSS and beginning of JS files along with set of questions in seperate js file

---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+        title: "Which reindeer is most famous of all?",
+        choices: ["Rudolph", "Blitzer", "Comet", "Dasher"],
+        answer: "Rudolph"
+    },
+    {
+        title: "What are hung up by the fireplace to fill with small gifts and candy?",
+        choices: [
+        "Buckets",
+        "Stockings",
+        "Bags",
+        "Pots"
+        ],
+        answer: "Stockings"
+    },
+    {
+        title: "Most asked for Christmas song?",
+        choices: ["Jingle Bells", "Feliz Navidad", "All I Want for Christmas", "Silent Night"],
+        answer: "All I Want for Christmas"
+    },
+    {
+        title: "How many ghosts show up in A Christmas Carol?",
+        choices: [
+        "Two",
+        "Four",
+        "Six",
+        "None of the Above"
+        ],
+        answer: "Four"
+    },
+    {
+        title: "What is Ralphie’s little brother’s name in the movie A Christmas Story?",
+        choices: ["Grover", "Scut", "Flick", "Randy"],
+        answer: "Randy"
+    },
+    {
+        title: "What are you supposed to do when you find yourself under the mistletoe?",
+        choices: [
+        "Drink",
+        "Kiss",
+        "Sing",
+        "all of the above"
+        ],
+        answer: "Kiss"
+    }
+    ];
---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+var startQuizEl = document.getElementById('start'); 
+var answer1El = document.getElementById('answer1'); 
+var answer2El = document.getElementById('answer2'); 
+var answer3El = document.getElementById('answer3'); 
+var answer4El = document.getElementById('answer4');
+var answer5El = document.getElementById('answer5');
+var answer6El = document.getElementById('answer6'); 
+var submitScoreEl = document.getElementById('submitScore'); 
+
+
+// Hide items that are not ready to be displayed
+answer1El.style.display = 'none';
+answer2El.style.display = 'none';
+answer3El.style.display = 'none';
+answer4El.style.display = 'none';
+answer5El.style.display = 'none';
+answer6El.style.display = 'none';
+submitScoreEl.style.display = 'none';
+enterInitials.style.display='none';
